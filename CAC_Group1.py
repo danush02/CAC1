f6cac49ea9361a02985cebcd2ac74803bedd8eb4
Thamizhanbu Elamurugu <30389038+danush02@users.noreply.github.com>
2023-09-19 17:28:19 +0530

    Code updated with correction in calculation

--- CAC_Group1.py
+++ CAC_Group1.py
@@ -46,6 +46,7 @@ def userLogin(role):
     os.system('cls')
     print("Login Page")
     print("_"*15)
+    name=None
     username=input("Username:")
     flag=False
     if role==1:
@@ -53,6 +54,7 @@ def userLogin(role):
             cred=uname.split(",")
             if cred[1]==username:
                 flag=True
+                name=cred[3]
         if flag==False:
             print("Username is incorrect")
             userLogin()
@@ -73,23 +75,24 @@ def userLogin(role):
     elif role==2:
         for uname in open("studentCredentials.csv","r+").readlines():
             cred=uname.split(",")
-            if cred[0]==username:
+            if cred[1]==username:
                 flag=True
         if flag==False:
             print("Username is incorrect")
-            userLogin()
+            userLogin(role)
         else:
             flag=False
             password=input("Password:")
             for passw in open("studentCredentials.csv","r+").readlines():
                 cred=passw.split(",")
-                if cred[1]==password:
+                if cred[2]==password:
                     flag=True
+                    name=cred[3]
             if flag==False:
                 print("Password is incorrect")
                 userLogin(role)
             else:
-                studentLandingPage()
+                studentLandingPage(name)
 
 def createNewUser(role):
     os.system('cls')
@@ -119,33 +122,33 @@ def createNewUser(role):
             file.close()
             userHomePage(role)
     elif role==2:
-        print("1.Create New User\n2.Delete account")
-        action=int(input("Select your action:"))
-        if action==1:
-            flag=False
-            name=input("Enter your Name:")
-            name=name.strip().title()
-            studid=input("Enter you Student ID:")
-            for line in open("studentCredentials.csv","r+").readlines():
-                id=line.split(",")
-                if id[0]==studid:
-                    flag=True
-            if flag:
-                print("User already exist")
-                nxt=input("Press enter to continue")
-                if nxt=="":
-                    createNewUser(role)
-            else:
-                department=["MSc Data Science","BA LLB","LLM","BSc Data Science","MBA","BCom"]
-                dept=department[int(input("1.MSc Data Science\n2.BA LLB\n3.LLM\n4.BSc Data Science\n5.MBA\n6.BCom\nSelect your Department:"))-1]
-                username=name[:4].lower()+str(random.randint(10,99))+str(random.randint(10,99))
-                print("Your Username is ",username)
-                password=name[:1].lower()+str(random.randint(0,9))+name[1:3]+str(random.randint(100,999))
-                print("Your Password is ",password)
-                file=open("studentCredentials.csv","a+")
-                file.write(studid+","+username+","+password+","+name+","+department+"\n")
-                file.close()
-                
+        flag=False
+        name=input("Enter your Name:")
+        name=name.strip().title()
+        studid=input("Enter you Student ID:")
+        for line in open("studentCredentials.csv","r+").readlines():
+            id=line.split(",")
+            if id[0]==studid:
+                flag=True
+        if flag:
+            print("User already exist")
+            nxt=input("Press enter to continue")
+            if nxt=="":
+                createNewUser(role)
+        else:
+            department=["MSc Data Science","BA LLB","LLM","BSc Data Science","MBA","BCom"]
+            dept=department[int(input("1.MSc Data Science\n2.BA LLB\n3.LLM\n4.BSc Data Science\n5.MBA\n6.BCom\nSelect your Department:"))-1]
+            username=name[:4].lower()+str(random.randint(10,99))+str(random.randint(10,99))
+            print("Your Username is ",username)
+            password=name[:1].lower()+str(random.randint(0,9))+name[1:3]+str(random.randint(100,999))
+            print("Your Password is ",password)
+            file=open("studentCredentials.csv","a+")
+            file.write(studid+","+username+","+password+","+name+","+dept+"\n")
+            file.close()
+            nxt=input("Press enter to continue")
+            if nxt=="":
+                userLogin(role)
+            
 def forgotPassword(role):
     os.system('cls')
     print("Password Recovery Page")
@@ -156,15 +159,13 @@ def forgotPassword(role):
             cred=i.split(",")
             if cred[1]==username:
                 print("Your password is ",cred[2])
-                name=cred[3]
-                adminLandingPage(name)
+                userLogin(role)
     elif role==2:
         for i in open("studentCredentials.csv","r+").readlines():
             cred=i.split(",")
             if cred[1]==username:
                 print("Your password is ",cred[2])
-                name=cred[3]
-                studentLandingPage(name)
+                userLogin(role)
 
 #Function for Landing Page of Admin after login
 def adminLandingPage(name):
@@ -306,28 +307,28 @@ def studentLandingPage(name):
     print("Student Page- Welcome! ",name)
     print("_"*20)
     print("Select your action:")
-    print("1.Create Order\n2.View Transactions\n3.Analyse Orders\n4.Logout")
+    print("1.Create Order\n2.View Transactions\n3.Logout")
     action=int(input("Your action:"))
     if action==1:
-        createOrder(0,0)
+        createOrder(0,0,name,[])
     elif action==2:
         viewTransactions()
     elif action==3:
-        analyseOrders()
-    elif action==4:
         userHomePage(1)
     else:
         os.system('cls')
         print("Enter a valid number\n")
         studentLandingPage(name)
 
-def createOrder(check,orderid):
+def createOrder(ch,orderid,name,order):
+    os.system('cls')
+    if ch==0:
+        order=[]
     print("Food Order")
     print("Today's Menu")
     print("-"*30)
     menu={"mainCourse":[],
     "snacks":[]}
-    os.system('cls')
     print("Current Menu")
     print("-"*15)
     slen=0
@@ -358,24 +359,48 @@ def createOrder(check,orderid):
     for i in menu["snacks"]:
         sno+=1
         print(str(sno)+" "*(3-len(str(sno))+3)+i[0]+" "*(slen-len(i[0])+4)+str(i[1]))
-    itemNum=int(input("Enter required item number:"))
-    itemQuantity=int(input("Enter required quantity:"))
-    if (itemNum-1)<=len(menu["mainCourse"]):
-        itemPrice=menu["mainCourse"][itemNum-1][1]
-        itemName=menu["mainCourse"][itemNum-1][0]
-    elif (itemNum-1)>len(menu["mainCourse"]):
-        itemPrice=menu["snacks"][itemNum-len(menu["mainCourse"])-1][1]
-        itemName=menu["snacks"][itemNum-len(menu["mainCourse"])-1][0]
-    if check==0:
-        orderid=generateOrderid()
-    file=open("orderDetails.csv","a+")
-    file.write(str(orderid)+","+itemName+","+str(itemQuantity)+","+str(itemPrice*itemQuantity))
-    createOrder(check+1,orderid)
+    itemNum=int(input("Enter required item number (Enter 0 to stop order):"))
+    if itemNum==0:
+        total=0
+        print("OrderID:",orderid)
+        for line in order:
+                print("Item:",line[1],"Quantity:",line[2],"Amount",line[3])
+                total=total+int(line[3])
+        print("Your total bill amount is ",total)
+        payment=input("Pay the bill amount and type 'paid':")
+        if payment=="paid":
+            paymentid=generatePaymentid()
+            print("Your transaction reference number is ",paymentid)
+            file=open("orderDetails.csv","a+")
+            for line in order:
+                print("Item:",line[1],"Quantity:",line[2],"Amount",line[3])
+                total=total+int(line[3])
+                file.write(line[0]+","+line[1]+","+str(line[2])+","+str(line[3])+","+str(paymentid))
+            file.close()
+            studentLandingPage(name)
+    else:
+        itemQuantity=int(input("Enter required quantity:"))
+        if (itemNum-1)<=len(menu["mainCourse"]):
+            itemPrice=menu["mainCourse"][itemNum-1][1]
+            itemName=menu["mainCourse"][itemNum-1][0]
+        elif (itemNum-1)>len(menu["mainCourse"]):
+            itemPrice=menu["snacks"][itemNum-len(menu["mainCourse"])-1][1]
+            itemName=menu["snacks"][itemNum-len(menu["mainCourse"])-1][0]
+        if ch==0:
+            orderid=generateOrderid()
+        order.append([orderid,itemName,itemQuantity,itemPrice*itemQuantity])
+        createOrder(ch+1,orderid,name,order)
 
 def generateOrderid():
     rand=[0]*5
     for i in range(0,5):
         rand[i]=str(random.randint(0,9))
     return rand[0]+rand[1]+rand[2]+rand[3]+rand[4]
+
+def generatePaymentid():
+    rand=[0]*6
+    for i in range(0,5):
+        rand[i]=str(random.randint(random.randint(0,5),random.randint(6,9)))
+    return rand[0]+rand[1]+rand[2]+rand[3]+rand[4]+rand[5]
 
 homePage()
